2ce21e3451757ce03804600678430098027059d7
iroliveira <iroliveira@viptech.com.br>
2023-12-02 14:57:01 -0300

    firts commit

--- src/main/java/com/example/trabalho/controller/StudentGradesController.java
+++ src/main/java/com/example/trabalho/controller/StudentGradesController.java
@@ -1,9 +1,14 @@
 package com.example.trabalho.controller;
 
+import com.example.trabalho.service.StudentGradesService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/grades")
 public class StudentGradesController {
+
+    @Autowired
+    private StudentGradesService service;
 }
